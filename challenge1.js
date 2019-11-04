57a40f3db29ec90a81c63ba35a5a70237167acd9
Pandassi <47264697+pandassi@users.noreply.github.com>
2019-11-03 21:34:06 -0500

    Add files via upload

---
+++ challenge1.js
@@ -0,0 +1,29 @@
+var volunteers = [
+    'Sally',
+    'Jake',
+    'Brian',
+    'Hamid'
+];
+
+var neighbourhoods = [
+    'Central Valley',
+    'Big Mountain',
+    'Little Bridge',
+    'Bricktown',
+    'Brownsville',
+    "Paul's Boutique",
+    'Fox Nest'
+];
+
+const doorToDoor = (volunteers, neighbourhoods) => {
+
+    let numVolunteers = volunteers.length;
+    let numNeighbourhoods = neighbourhoods.length;
+    let split = numNeighbourhoods / numVolunteers;
+    return split
+
+    // Remember to return a value
+
+    // Want to see what's going on inside your function? You can use console.log() to debug and print variables to the console below.
+
+}
---
+++ challenge3.js
@@ -0,0 +1,15 @@
+const castVote = (name, votes) => {
+    switch (name) {
+        case "Tim":
+            votes[0]++;
+            break;
+        case "Sally":
+            votes[1]++;
+            break;
+        case "Beth":
+            votes[2]++;
+            break;
+    }
+
+    return votes;
+}
