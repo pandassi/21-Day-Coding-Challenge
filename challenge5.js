e11b2c4e373d19008d8a94b9acd31ce5c409678b
Pandassi <47264697+pandassi@users.noreply.github.com>
2019-11-05 12:10:46 -0500

    Add files via upload

---
+++ challenge5.js
@@ -0,0 +1,23 @@
+
+// goodStations = at least 20 capacity
+// goodStations = can be schools OR community centers
+
+const stations = [
+    ['Big Bear Donair', 10, 'restaurant'],
+    ['Bright Lights Elementary', 50, 'school'],
+    ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+function chooseStations(stations) {
+    const goodStations = []
+
+    for (const station of stations) {
+        const capacity = station[1]
+        const type = station[2]
+        if (capacity >= 20 && (type === "school" || type === "community centre")) {
+            goodStations.push(station[0])
+        }
+    }
+    return goodStations
+}
+console.log(chooseStations(stations))
