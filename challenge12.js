70e21e2c08840b02b8fb69cf3b96a5ba87b99f09
Pandassi <47264697+pandassi@users.noreply.github.com>
2019-11-12 14:20:29 -0500

    Add files via upload

---
+++ challenge12.js
@@ -0,0 +1,15 @@
+
+const checkAir = function (samples, threshold) {
+    dirtyCounter = 0;
+
+    for (const sample of samples) {
+        if (sample === "dirty") {
+            dirtyCounter++;
+        }
+    }
+    if (dirtyCounter / samples.length > threshold) {
+        return "Polluted"
+    } else {
+        return "Clean"
+    }
+}
