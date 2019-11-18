1a1622c62f31c2b1cefbe7d26d3b02370e9d4e47
Pandassi <47264697+pandassi@users.noreply.github.com>
2019-11-17 20:26:28 -0500

    Add files via upload

---
+++ challenge17.js
@@ -0,0 +1,13 @@
+const judgeVegetable = (vegetables, metric) => {
+  let bestVegetable = {
+    submitter: "",
+    metricValue: 0
+  };
+  for (const vegetable of vegetables) {
+    if (vegetable[metric] > bestVegetable.metricValue) {
+      bestVegetable.submitter = vegetable.submitter;
+      bestVegetable.metricValue = vegetable[metric];
+    }
+  }
+  return bestVegetable.submitter;
+};
